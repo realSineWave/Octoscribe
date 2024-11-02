27c771e59e0a07337d38879d289822f2649b0b42
Edison Zeng <edison.zeng@mail.utoronto.ca>
2024-11-02 16:47:06 -0400

    Add design for AddFileView

---
+++ src/main/java/ca/axoplasm/Octoscribe/view/AddFileView.java
@@ -0,0 +1,11 @@
+package ca.axoplasm.Octoscribe.view;
+
+import javax.swing.*;
+
+public class AddFileView {
+    private JPanel panel1;
+    private JButton settingsButton;
+    private JButton aboutButton;
+    private JButton chooseButton;
+    private JButton addButton;
+}
