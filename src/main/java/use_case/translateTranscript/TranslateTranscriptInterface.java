54ce07c31c6445625ac1ef1e08d22b21361c10f1
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-11-07 17:18:17 -0500

    new functions to translate transcription

--- src/main/java/use_case/translateTranscript/TranslateTranscriptInterface.java
+++ src/main/java/use_case/translateTranscript/TranslateTranscriptInterface.java
@@ -1,8 +1,13 @@
 package use_case.translateTranscript;
 
+import ca.axoplasm.Octoscribe.entity.Transcription;
+
 public interface TranslateTranscriptInterface {
     /**
      * Translate the transcript into another language
      */
+    Transcription translate();
+
+
 
 }
