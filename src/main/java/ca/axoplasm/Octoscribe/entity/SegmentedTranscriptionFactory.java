25a00aed984725055a0e745b202f4c4ffd04a7c1
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-11-09 16:58:54 -0500

    Added factory for entities.

---
+++ src/main/java/ca/axoplasm/Octoscribe/entity/SegmentedTranscriptionFactory.java
@@ -0,0 +1,10 @@
+package ca.axoplasm.Octoscribe.entity;
+
+import java.util.List;
+
+public class SegmentedTranscriptionFactory implements TransFactory{
+    @Override
+    public SegmentedTranscription createSegmented(String language, String text, List<Segment> segment) {
+        return new SegmentedTranscription(language, text, segment);
+    }
+}
