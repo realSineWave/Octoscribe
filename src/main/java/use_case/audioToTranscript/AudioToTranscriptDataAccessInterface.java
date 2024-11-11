c79de39395b81c19280598f98b64b409965ddfc0
Evan <evan@Evans-MBP.ht.home>
2024-11-10 22:10:16 -0500

    adds file save interface

---
+++ src/main/java/use_case/audioToTranscript/AudioToTranscriptDataAccessInterface.java
@@ -0,0 +1,4 @@
+package use_case.audioToTranscript;
+
+public interface AudioToTranscriptDataAccessInterface {
+}
---
+++ src/main/java/use_case/audioToTranscript/AudioToTranscriptFileSaveInterface.java
@@ -0,0 +1,8 @@
+package use_case.audioToTranscript;
+
+import entity.SegmentedTranscription;
+
+public interface AudioToTranscriptFileSaveInterface {
+
+void save (SegmentedTranscription segmentedTranscription);
+}
