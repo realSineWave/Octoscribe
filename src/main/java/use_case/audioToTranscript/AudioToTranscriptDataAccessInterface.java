763a2863da95c1d46cede30240f3020be04f86d5
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-11-18 17:43:51 -0500

    Delete all initializer, use the DAO with parameters like file or target language

--- src/main/java/use_case/audioToTranscript/AudioToTranscriptDataAccessInterface.java
+++ src/main/java/use_case/audioToTranscript/AudioToTranscriptDataAccessInterface.java
@@ -4,6 +4,7 @@ import ca.axoplasm.Octoscribe.entity.Segment;
 import ca.axoplasm.Octoscribe.entity.SegmentedTranscription;
 
 import javax.json.JsonObject;
+import java.io.File;
 import java.util.List;
 
 /**
@@ -14,7 +15,7 @@ public interface AudioToTranscriptDataAccessInterface {
     /**
      * Get the translated result in format of JSObject
      */
-    JsonObject getTranscriptedJson();
+    JsonObject getTranscriptedJson(File file);
 
     /**
      * Output segments
@@ -24,5 +25,5 @@ public interface AudioToTranscriptDataAccessInterface {
     List<Segment> toSegments(JsonObject jsonObject);
 
 
-    SegmentedTranscription getSegmentedTranscription();
+    SegmentedTranscription getSegmentedTranscription(File file);
 }
