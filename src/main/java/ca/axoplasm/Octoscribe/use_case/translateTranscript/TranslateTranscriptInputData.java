c05c5584f219d91ef713264be49a1590af626953
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-11-19 15:18:59 -0500

    added translate transcription

---
+++ src/main/java/ca/axoplasm/Octoscribe/use_case/translateTranscript/TranslateTranscriptInputData.java
@@ -0,0 +1,23 @@
+package use_case.translateTranscript;
+
+import ca.axoplasm.Octoscribe.entity.SegmentedTranscription;
+
+import java.io.File;
+
+public class TranslateTranscriptInputData {
+    private final SegmentedTranscription segmentedTranscription;
+    private final String targetLanguage;
+
+    public TranslateTranscriptInputData(SegmentedTranscription segmentedTranscription, String targetLanguage){
+        this.segmentedTranscription = segmentedTranscription;
+        this.targetLanguage = targetLanguage;
+    }
+
+    public String getTargetLanguage(){
+        return this.targetLanguage;
+    }
+
+    public SegmentedTranscription getSegmentedTranscription(){
+        return this.segmentedTranscription;
+    }
+}
