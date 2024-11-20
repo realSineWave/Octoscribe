0ef816ede8e5afed50a2af083425b36efc6a0e7e
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-11-19 19:35:03 -0500

    Implemented all parts in TranslateTranscript, preparing for merge, waiting for view model

---
+++ src/main/java/data_access/TranslateTranscriptFileSaveObject.java
@@ -0,0 +1,9 @@
+package data_access;
+
+import use_case.translateTranscript.TranslateTranscriptDataAccessInterface;
+import use_case.translateTranscript.TranslateTranscriptInteractor;
+import 
+
+public class TranslateTranscriptionFileSaveObject implements TranslateTranscriptDataAccessInterface {
+    private String name = "translated_subtitle_at{}"
+}
